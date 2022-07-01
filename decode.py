196e6ee3a4602aa653f6a6c7de714508cc6dafd4
shahid <m.shahidmuneer88@gmail.com>
2022-07-01 18:03:31 +0500

    Added results

--- decode.py
+++ decode.py
@@ -1,6 +1,3 @@
-from rivagan import RivaGAN
-
-model = RivaGAN.load("data/model.pt")
-# print(model)
-recovered_data = model.decode("deepfake.avi")
-print(next(recovered_data))
+version https://git-lfs.github.com/spec/v1
+oid sha256:74fac8748def218fca13c76528aa46590b824681cbad930e6ca5458c5d5d8f29
+size 190
--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-# from rivagan import RivaGAN
-# model = RivaGAN()
-# model.fit("drive/MyDrive/RivaGAN/data/hollywood",batch_size=32, epochs=50)
-# model.save("./data/model.pt")
-
-from rivagan import RivaGAN
-model = RivaGAN()
-model.fit("/kaggle/input/ucf101/UCF101/UCF-101/",batch_size=32, epochs=75)
-# model.fit("/kaggle/input/ucfclipped/UCF-101/",batch_size=32, epochs=40)
-# model.fit("data/hollywood",batch_size=32, epochs=40)
-# model.fit("/kaggle/input/hmdb51/HMDB51",batch_size=32, epochs=20)
-model.save("./model.pt")
+version https://git-lfs.github.com/spec/v1
+oid sha256:66e52888cb924ab4fa21f1c4ead431ac5f79fa8e289e427e1044cda13d6b118a
+size 511
